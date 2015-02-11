aea386ca5e4936f3f8446cbc5cd5a08e03964bfc
Ed van Bruggen <edvb54@gmail.com>
2015-02-10 18:59:19 -0800

    Fix doors not opening and closing

--- src/item.c
+++ src/item.c
@@ -41,18 +41,19 @@ void alloc_item(int from, int to) {
 }
 
 void toggle_door(int x, int y) {
-	if (get_map(y-1, x) == '+') set_map_char(y-1, x, '-');
-	else if (get_map(y+1, x) == '+') set_map_char(y+1, x, '-');
-	else if (get_map(y, x-1) == '+') set_map_char(y, x-1, '-');
-	else if (get_map(y, x+1) == '+') set_map_char(y, x+1, '-');
-	else if (get_map(y-1, x) == '-') set_map_char(y-1, x, '+');
-	else if (get_map(y+1, x) == '-') set_map_char(y+1, x, '+');
-	else if (get_map(y, x-1) == '-') set_map_char(y, x-1, '+');
-	else if (get_map(y, x+1) == '-') set_map_char(y, x+1, '+');
+	if (get_map(x-1, y) == '+') set_map_char(x-1, y, '-');
+	else if (get_map(x+1, y) == '+') set_map_char(x+1, y, '-');
+	else if (get_map(x, y-1) == '+') set_map_char(x, y-1, '-');
+	else if (get_map(x, y+1) == '+') set_map_char(x, y+1, '-');
+	else if (get_map(x-1, y) == '-') set_map_char(x-1, y, '+');
+	else if (get_map(x+1, y) == '-') set_map_char(x+1, y, '+');
+	else if (get_map(x, y-1) == '-') set_map_char(x, y-1, '+');
+	else if (get_map(x, y+1) == '-') set_map_char(x, y+1, '+');
 
 	draw_map();
 	for (int i = 0; i <= itemCount; i++)
 		draw_item(item[i]);
+
 }
 
 void clear_item(item_t *item, int x, int y) {
