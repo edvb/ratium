ba71743e28c92f7aca02b056153e6e6da88c9cc6
Ed van Bruggen <edvb54@gmail.com>
2015-07-01 15:26:17 -0700

    Update config.def.h

--- config.def.h
+++ config.def.h
@@ -1,4 +1,4 @@
 /* settings for ratium */
 #define DEF_PLAYERS 1 /* default number of players */
-#define RAT_GFX NCURSES /* type of graphics *
-                         * right now can only be NCURSES, will soon add SDL */
+#define RAT_NCURSES /* type of graphics *
+                     * right now can only be NCURSES, will soon add SDL */
