d8b73786836949182921795a22e7bc8f921a0e61
Ed van Bruggen <edvb54@gmail.com>
2015-09-19 21:44:38 -0700

    Update misc/vim.sh

--- misc/vim.sh
+++ misc/vim.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 vim src/ratium.h src/main.c \
-    -O2 src/maps.h src/map.c src/item.c src/ent.h src/ent.c src/ai.c src/player.c \
+    -O2 src/game.c src/maps.h src/map.c src/item.c src/ent.h src/ent.c src/ai.c src/player.c \
     src/data.c src/gfx.h src/gfx.c
 
