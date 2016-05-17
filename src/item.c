a1b6b7cf58fc0d4e6bdd408c7f27f8374e62051c
Ed van Bruggen <edvb54@gmail.com>
2016-05-16 21:54:34 -0700

    Fix query_item to return -1 for a NULL name

--- src/item.c
+++ src/item.c
@@ -5,6 +5,8 @@
 
 /* query_item: return element number of array matching name supplied */
 int query_item(char *name) {
+	if (name == NULL)
+		return -1;
 	for (int i = 0; i <= itemqty; i++)
 		if (strcmp(item[i].name, name) == 0)
 			return i;
