c115e1b07f38b16e74644617132f8250eed82708
Ed van Bruggen <edvb54@gmail.com>
2016-06-18 22:20:02 -0700

    Add LEN macro to util.h

--- src/util.h
+++ src/util.h
@@ -1,6 +1,8 @@
 #ifndef UTIL_H
 #define UTIL_H
 
+#define LEN(X) (sizeof(X) / sizeof((X)[0]))
+
 int estrcmp(const char *s1, const char *s2);
 char  *strdup(const char *s);
 char **str_split(const char *s, const char a_delim);
